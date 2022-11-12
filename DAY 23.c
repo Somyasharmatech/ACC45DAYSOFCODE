483b710bd43bdfbb8b259aea0d802f704c575ddf
Somyasharmatech <114328620+Somyasharmatech@users.noreply.github.com>
2022-11-12 20:46:20 +0530

    Add files via upload

---
+++ DAY 23.c
@@ -0,0 +1,16 @@
+#include<string.h>
+void main()
+{
+    int i;
+    char str[20];
+    printf("enter a string");
+    gets(str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>='A'&& str[i]<='Z')
+        {
+            str[i]+=32;
+        }
+    }
+    printf("str in lower case is %s",str);
+}
