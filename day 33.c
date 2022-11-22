685a9f5e8fe164c7878a7c2c7073886bb74fd77d
Somyasharmatech <114328620+Somyasharmatech@users.noreply.github.com>
2022-11-22 21:57:45 +0530

    Add files via upload

---
+++ day 33.c
@@ -0,0 +1,22 @@
+
+ //fibonacci series through funtion.
+#include<stdio.h>
+void fib(int);
+int main()
+{
+    int a,f;
+    printf("enter a number\n");
+    scanf("%d",&a);
+    fib(a);
+    return 0;
+}
+void fib(int n){
+    int x=0,y=1, temp,i;
+    printf("%d\n",x);
+    for (i=2;i<=n;i++){
+       temp=x;
+       x=y;
+       y=temp+y;
+       printf("%d\n",x);
+    }
+}
